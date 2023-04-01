26a89d54564f3394efd984e0bec5d4c222656944
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-02 00:39:27 +0200

    Creación TDD y clase Coche.

---
+++ MiPrimerTDD/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author Vicfa
+ *
+ */
+module MiPrimerTDD {
+	requires org.junit.jupiter.api;
+	requires jdk.incubator.vector;
+	requires junit;
+}
---
+++ MiPrimerTDD/src/tests/TestCoche.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCoche {
+
+	@Test
+	public void test_crear_coche() {
+		Coche nuevoCoche = new Coche();
+	}
+
+}
