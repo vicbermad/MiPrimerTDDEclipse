b2718bd4ed97f57ce6a8fea3ac67459f311e18a4
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-02 12:44:41 +0200

    Solución velocidad negativa.

--- MiPrimerTDD/src/tests/Coche.java
+++ MiPrimerTDD/src/tests/Coche.java
@@ -11,6 +11,8 @@ public class Coche {
 
 	public void decelerar(int deceleracion) {
 		velocidad -= deceleracion;
+		if (velocidad < 0)
+			velocidad = 0;
 	}
 
 }
--- MiPrimerTDD/src/tests/TestCoche.java
+++ MiPrimerTDD/src/tests/TestCoche.java
@@ -25,5 +25,12 @@ class TestCoche {
 		nuevoCoche.decelerar(20);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
+	@Test
+	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.velocidad = 50;
+		nuevoCoche.decelerar(80);
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
+	}
 
 }
