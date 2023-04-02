77f078fb8a3d9988888f906aba68ba57bad2c54f
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-02 12:24:28 +0200

    Métodos acelerar y decelerar.

--- MiPrimerTDD/src/tests/Coche.java
+++ MiPrimerTDD/src/tests/Coche.java
@@ -4,4 +4,13 @@ public class Coche {
 
 	public int velocidad;
 
+	public void acelerar(int aceleracion) {
+		velocidad += aceleracion;
+		
+	}
+
+	public void decelerar(int deceleracion) {
+		velocidad -= deceleracion;
+	}
+
 }
